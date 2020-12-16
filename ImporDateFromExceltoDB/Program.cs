256d043c3b42aa1066e24a18fc16090accd526ee
WMSLL <62074815+WMSLL@users.noreply.github.com>
2020-12-16 12:53:20 +0300

    Добавьте файлы проекта.

---
+++ ImporDateFromExceltoDB/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.SqlClient;
+using Excel = Microsoft.Office.Interop.Excel;
+namespace ImporDateFromExceltoDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ConnectionString = "Data Source =172.27.1.26; Initial Catalog = ILS; User ID =manh; Password =1q2w#E$R";
+            var sqlExpression = "insert into ils.dbo.TestImportExel (item,code) values('TTTT','12312312')";
+            SqlConnection sqlConnect = new SqlConnection(ConnectionString);
+            sqlConnect.Open();          
+
+            SqlCommand command = new SqlCommand(sqlExpression, sqlConnect);
+            command.ExecuteNonQuery();
+
+          
+        }
+    }
+}
