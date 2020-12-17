3a109675aa6bb35e0d7e86552ac9888524c2f5e2
WMSLL <62074815+WMSLL@users.noreply.github.com>
2020-12-17 13:52:30 +0300

    Сделал инсерт из уэкселя в ДБ

--- ImporDateFromExceltoDB/Program.cs
+++ ImporDateFromExceltoDB/Program.cs
@@ -1,5 +1,9 @@
-﻿using System.Data.SqlClient;
+﻿using System.Data;
+using System;
+using System.Data.SqlClient;
 using Excel = Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+
 namespace ImporDateFromExceltoDB
 {
     class Program
@@ -7,14 +11,46 @@ namespace ImporDateFromExceltoDB
         static void Main(string[] args)
         {
             string ConnectionString = "Data Source =172.27.1.26; Initial Catalog = ILS; User ID =manh; Password =1q2w#E$R";
-            var sqlExpression = "insert into ils.dbo.TestImportExel (item,code) values('TTTT','12312312')";
+
             SqlConnection sqlConnect = new SqlConnection(ConnectionString);
-            sqlConnect.Open();          
+            sqlConnect.Open();
 
-            SqlCommand command = new SqlCommand(sqlExpression, sqlConnect);
-            command.ExecuteNonQuery();
 
-          
+           var dataRage= ReadExel();
+
+            foreach (DataRow dr in dataRage.Rows)
+            {
+               // Console.WriteLine($"values({dr[0]},{dr[1]}");
+               var sqlExpression = $"insert into ils.dbo.TestImportExel (item,code) values('{dr[0]}','{dr[1]}')";
+               SqlCommand command = new SqlCommand(sqlExpression, sqlConnect);
+               command.ExecuteNonQuery();
+            }
+
+            static DataTable ReadExel()
+            {
+                Excel.Application application = new Excel.Application();
+                DataTable myTable = new DataTable("MyDataTable");
+                if (application == null)
+                {
+                    Console.WriteLine("EXCEL not installed");
+                    return myTable;
+                }
+                Excel.Workbook excelBook = application.Workbooks.Open(@"C:\Проэкты MS VS\Коледино.xlsx");
+                Excel._Worksheet excelSheet = application.Sheets[1];
+                Excel.Range excelRange = excelSheet.UsedRange;
+                int rows = excelRange.Rows.Count;
+                int cols = excelRange.Columns.Count;
+                myTable.Columns.Add("FirstName", typeof(string));
+                myTable.Columns.Add("LastName", typeof(string));
+                for (int i = 2; i <= rows; i++)
+                {
+                    DataRow myNewRow = myTable.NewRow();
+                    myNewRow["FirstName"] = excelRange.Cells.Value2[i, 1]; // .ToString(); //string
+                    myNewRow["LastName"] = excelRange.Cells.Value2[i, 2];
+                    myTable.Rows.Add(myNewRow);
+                }
+                return myTable;
+            }
         }
     }
 }
