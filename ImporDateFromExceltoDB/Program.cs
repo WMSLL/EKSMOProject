f7fef2833c6c305159c97af50298ee4a89aed35d
WMSLL <62074815+WMSLL@users.noreply.github.com>
2020-12-25 11:02:22 +0300

    Создал отдельный класс для чтения еэксель-файла

--- ImporDateFromExceltoDB/Program.cs
+++ ImporDateFromExceltoDB/Program.cs
@@ -20,32 +20,25 @@ namespace ImporDateFromExceltoDB
         {
             sqlConnect.Open();
             Timer timer = new Timer(TimerCallback, null, 0, 1_800_000);
-           
+
+
+
             Console.ReadLine();
             static void TimerCallback(Object o)
             {
-                
+                ReadExecss readExecss = new ReadExecss();
                 Console.WriteLine("Start");
                 var dir = new DirectoryInfo(Folder2); // папка с файлами 
 
                 foreach (FileInfo file in dir.GetFiles())
                 {
-                    if (file.Name!=null)
-                    {
-                        
-                    }
-                    //Console.WriteLine();
                     DateTimeOffset date = DateTimeOffset.Now;
-
                     Console.WriteLine($"считываем EXCEL");
                     var filename = Path.GetFileNameWithoutExtension(file.Name);
-
-                    var dataRage = ReadExel(Folder2 + filename + ".xlsx");
-
-
+                    readExecss.ReadExel(Folder2 + filename + ".xlsx");
+                    var dataRage = readExecss.Data;
                     foreach (DataRow dr in dataRage.Rows)
-                    {
-                        // var sqlExpression = $"insert into ils.dbo.TestImportExel (item,code) values('{dr[0]}','{dr[1]}')";
+                    {                        
                         var sqlExpression = $@"if Exists(Select *From [EKS_OrderSSCCChildrenWorld] where item='{dr[0]}' and [code]='{dr[1]}' ) begin
                                                                                                            return
                                                                                                            end 
@@ -55,8 +48,7 @@ namespace ImporDateFromExceltoDB
                                                                                end";
                         SqlCommand command = new SqlCommand(sqlExpression, sqlConnect);
                         command.ExecuteNonQuery();
-                    }
-                    //Thread.Sleep(5000);
+                    }                   
                     Console.WriteLine($"Убиваем процесс EXCEL");
                     System.Diagnostics.Process[] processes = System.Diagnostics.Process.GetProcessesByName("Excel");
                     foreach (var p in processes)
@@ -72,15 +64,14 @@ namespace ImporDateFromExceltoDB
                                 throw;
                             }
                         }
-                    }
-                   // Thread.Sleep(3000);
+                    }                    
                     Console.WriteLine($"Начинаем перемещать файл");
                     try
                     {
                         File.Move(Folder2 + filename + ".xlsx", newFolder + date.ToString().Replace(":", "_").Replace(" ", "") + ".xlsx");
-                        Console.WriteLine(Folder2 + filename + ".xlsx"+" Перемещен");
+                        Console.WriteLine(Folder2 + filename + ".xlsx" + " Перемещен");
                     }
-                    catch (System.IO.FileNotFoundException) 
+                    catch (System.IO.FileNotFoundException)
                     {
                         Console.WriteLine($"В папке {Folder2 + filename + ".xlsx"} нет файла");
                     }
@@ -88,48 +79,7 @@ namespace ImporDateFromExceltoDB
                 Console.WriteLine("Стоп");
                 Console.ReadKey();
             }
-            static DataTable ReadExel(string filename)
-            {
-                Excel.Application application = new Excel.Application();
-                DataTable myTable = new DataTable("MyDataTable");
-                if (application == null)
-                {
-                    Console.WriteLine("EXCEL not installed");
-                    return myTable;
-                }
-                try
-                {
-                    Excel.Workbook excelBook = application.Workbooks.Open(filename);
-                    Excel._Worksheet excelSheet = application.Sheets[1];
-                    Excel.Range excelRange = excelSheet.UsedRange;
-                    int rows = excelRange.Rows.Count;
-                    int cols = excelRange.Columns.Count;
-                    myTable.Columns.Add("FirstName", typeof(string));
-                    myTable.Columns.Add("LastName", typeof(string));
-                    for (int i = 2; i <= rows; i++)
-                    {
-                        DataRow myNewRow = myTable.NewRow();
-                        myNewRow["FirstName"] = excelRange.Cells.Value2[i, 1].ToString(); // .ToString(); //string
-                        myNewRow["LastName"] = excelRange.Cells.Value2[i, 2].ToString();
-                        myTable.Rows.Add(myNewRow);
-                    }
-                    if (excelBook != null)
-                    {
-                        excelBook.Close(false, Type.Missing, Type.Missing);
-                        application.Workbooks.Close();
-                        System.Runtime.InteropServices.Marshal.ReleaseComObject(excelBook);
-                        application.Quit();
-                        GC.Collect();
-                        System.Runtime.InteropServices.Marshal.ReleaseComObject(application);
-                    }
-                }
-                catch (System.Runtime.InteropServices.COMException)
-                {
-
-                    Console.WriteLine($"В папке {filename} нет файла");
-                }
-                return myTable;
-            }
         }
     }
+
 }
---
+++ ImporDateFromExceltoDB/ReadExecss.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Data;
+using Excel = Microsoft.Office.Interop.Excel;
+namespace ImporDateFromExceltoDB
+{
+    
+    class ReadExecss
+    {
+        public  DataTable Data { get; set; }
+       
+
+        public void ReadExel(string filename)
+        {
+            Excel.Application application = new Excel.Application();
+            DataTable myTable = new DataTable("MyDataTable");
+            if (application == null)
+            {
+                Console.WriteLine("EXCEL not installed");
+                Data= myTable;
+            }
+            try
+            {
+                Excel.Workbook excelBook = application.Workbooks.Open(filename);
+                Excel._Worksheet excelSheet = application.Sheets[1];
+                Excel.Range excelRange = excelSheet.UsedRange;
+                int rows = excelRange.Rows.Count;
+                int cols = excelRange.Columns.Count;
+                myTable.Columns.Add("FirstName", typeof(string));
+                myTable.Columns.Add("LastName", typeof(string));
+                for (int i = 2; i <= rows; i++)
+                {
+                    DataRow myNewRow = myTable.NewRow();
+                    myNewRow["FirstName"] = excelRange.Cells.Value2[i, 1].ToString(); // .ToString(); //string
+                    myNewRow["LastName"] = excelRange.Cells.Value2[i, 2].ToString();
+                    myTable.Rows.Add(myNewRow);
+                }
+                if (excelBook != null)
+                {
+                    excelBook.Close(false, Type.Missing, Type.Missing);
+                    application.Workbooks.Close();
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(excelBook);
+                    application.Quit();
+                    GC.Collect();
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(application);
+                }
+            }
+            catch (System.Runtime.InteropServices.COMException)
+            {
+
+                Console.WriteLine($"В папке {filename} нет файла");
+            }
+            Data= myTable;
+        }
+    }
+}
