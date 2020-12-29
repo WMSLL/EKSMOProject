b183a0dd8bfab1993e3a227d2dcb1d065401b3d8
WMSLL <62074815+WMSLL@users.noreply.github.com>
2020-12-29 15:24:33 +0300

    linq lessons

---
+++ LinqLesson/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] FootbolClub = { "Бавария","Манчестер сити","Ливерпуль","Реал Мадрит","ПСЖ","Барселона"};
+
+            var itemFC = from t in FootbolClub
+                         where t.ToUpper().StartsWith("Б")
+                         orderby t
+                         select t;
+            foreach (var item in itemFC)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
