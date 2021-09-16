b95a23a5269e816bdc121574f09a821ee5d83b14
Виктор Кизлик <kizlik1108@Gmail.com>
2021-09-16 10:29:32 +0300

    Убрал возможность прокачиватся с различными типами пробелов типа CHAR(160), CHAR(13). CHAR(9), CHAR(10)

--- ImporDateFromExceltoDB/Program.cs
+++ ImporDateFromExceltoDB/Program.cs
@@ -5,13 +5,14 @@ using System.Data.SqlClient;
 using Excel = Microsoft.Office.Interop.Excel;
 using System.Threading;
 
+
 namespace ImporDateFromExceltoDB
 {
     class Program
     {
         static string connectionString = @"Data Source=172.27.1.25\SRVSQL;Initial Catalog=ILS;Persist Security Info=True;User ID=szkoadmin;Password=#t1h2u3$;";
         static string Folder2 = @"\\w-srvfile\wailberis\";
-        static string newFolder = @"\\w-srvfile\wailberis\old_wailberis\Коледино";
+        static string newFolder = @"\\w-srvfile\wailberis\old_wailberis\";
         static SqlConnection sqlConnect = new SqlConnection(connectionString);
         static void Main(string[] args)
         {
@@ -27,26 +28,26 @@ namespace ImporDateFromExceltoDB
                 {
                     DateTimeOffset date = DateTimeOffset.Now;
                     var typeFile = Path.GetExtension(file.FullName);
-
-
                     var filename = Path.GetFileNameWithoutExtension(file.Name);
                     if (typeFile == ".xlsx" || typeFile == ".xls")
                     {
-
-
                         Console.WriteLine($"Read EXCEL");
                         readExecss.ReadExel(Folder2 + filename + typeFile);
                         var dataRage = readExecss.Data;
                         foreach (DataRow dr in dataRage.Rows)
                         {
-                            var sqlExpression = $@"if Exists(Select *From [EKS_OrderSSCCChildrenWorld] where item=Ltrim(Rtrim('{dr[0]}')) and [code]=Ltrim(Rtrim('{dr[1]}')) ) begin
+                            var t0 = dr[0].ToString();
+                            var t1 = dr[1].ToString();
+                            var t2 = dr[2].ToString();
+                            var sqlExpression = $@"if Exists(Select *From [EKS_OrderSSCCChildrenWorld] where item=LTRIM(RTRIM(REPLACE(REPLACE(REPLACE(REPLACE('{t0}', CHAR(10), ''), CHAR(13), ''), CHAR(9), ''), CHAR(160), ''))) and [code]=LTRIM(RTRIM(REPLACE(REPLACE(REPLACE(REPLACE('{t1}', CHAR(10), ''), CHAR(13), ''), CHAR(9), ''), CHAR(160), ''))) ) begin
                                                                                                            return
                                                                                                            end 
                                                                                else
                                                                                begin
-                                                                               insert into [EKS_OrderSSCCChildrenWorld] ([item],	[code],[OrderId] ) values (Ltrim(Rtrim('{dr[0]}')),Ltrim(Rtrim('{dr[1]}')) ,Ltrim(Rtrim('{dr[2]}')) )
+                                                                               insert into [EKS_OrderSSCCChildrenWorld] ([item],	[code],[OrderId] ) values (LTRIM(RTRIM(REPLACE(REPLACE(REPLACE(REPLACE('{t0}', CHAR(10), ''), CHAR(13), ''), CHAR(9), ''), CHAR(160), ''))),
+                                                                              LTRIM(RTRIM(REPLACE(REPLACE(REPLACE(REPLACE('{t1}', CHAR(10), ''), CHAR(13), ''), CHAR(9), ''), CHAR(160), ''))) ,LTRIM(RTRIM(REPLACE(REPLACE(REPLACE(REPLACE('{t2}', CHAR(10), ''), CHAR(13), ''), CHAR(9), ''), CHAR(160), ''))) )
                                                                                end";
-                            Console.WriteLine($"item: Ltrim(Rtrim('{dr[0]}')), Code: Ltrim(Rtrim('{dr[1]}')) ,Orders: Ltrim(Rtrim('{dr[2]}')) ");
+                            Console.WriteLine($"item: '{t0}', Code: '{t1}' ,Orders: '{t2}' ");
                             SqlCommand command = new SqlCommand(sqlExpression, sqlConnect);
                             command.ExecuteNonQuery();
                         }
@@ -70,12 +71,12 @@ namespace ImporDateFromExceltoDB
                         Console.WriteLine($"start transfer file");
                         try
                         {
-                            File.Move(Folder2 + filename + typeFile, newFolder + date.ToString().Replace(":", "_").Replace(" ", "") + typeFile);
-                            Console.WriteLine(Folder2 + filename + ".xlsx" + " Transfer complict");
+                            File.Move(Folder2 + filename + typeFile, newFolder + filename + date.ToString().Replace(":", "_").Replace(" ", "") + typeFile);
+                            Console.WriteLine(Folder2 + filename + typeFile + " Transfer complict");
                         }
                         catch (System.IO.FileNotFoundException)
                         {
-                            Console.WriteLine($"In folder {Folder2 + filename + ".xlsx"} file not faund");
+                            Console.WriteLine($"In folder {Folder2 + filename + typeFile} file not faund");
                         }
                     }
                 }
