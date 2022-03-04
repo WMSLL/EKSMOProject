22a97e513ff23c94a266a386a1982a3eae07acb1
Виктор Кизлик <kizlik1108@Gmail.com>
2022-03-04 15:07:57 +0300

    safdfsfd

--- CLR_SVGToZPL/Class1.cs
+++ CLR_SVGToZPL/Class1.cs
@@ -1,9 +1,37 @@
 ﻿using System;
-
+using System.Linq;
+using Svg;
+using Svg.Contrib.Render;
+using Svg.Contrib.Render.ZPL;
 
 namespace CLR_SVGToZPL
 {
-    public class Class1
+{
+    static class Class1
     {
+
+        public static void Zpl22(string file)
+        {
+            // var file = "";
+            
+            var svgDocument = SvgDocument.Open(file);
+            var bootstrapper = new DefaultBootstrapper();
+            var zplTransformer = bootstrapper.CreateZplTransformer();
+            var zplRenderer = bootstrapper.CreateZplRenderer(zplTransformer,
+                                                             characterSet: CharacterSet.ZebraCodePage850);
+            var viewMatrix = bootstrapper.CreateViewMatrix(zplTransformer,
+                                                           sourceDpi: 90f,
+                                                           destinationDpi: 203f,
+                                                           viewRotation: ViewRotation.Normal);
+            var zplContainer = zplRenderer.GetTranslation(svgDocument,
+                                                          viewMatrix);
+
+
+           
+
+            var encoding = zplRenderer.GetEncoding();
+            var array = zplContainer.ToByteStream(encoding)
+                                    .ToArray();
+        }
     }
 }
--- SVGtoZPL22/Program.cs
+++ SVGtoZPL22/Program.cs
@@ -2,8 +2,6 @@
 using System.Linq;
 using System.Text;
 using Svg;
-using Svg.Contrib.Render;
-using Svg.Contrib.Render.ZPL;
 
 
 namespace SVGtoZPL22
@@ -12,28 +10,44 @@ namespace SVGtoZPL22
     {
         static void Main(string[] args)
         {
-            var file = "C:\\Users\\Виктор\\OneDrive\\Рабочий стол\\Текстовый документ (2).SVG";
+            var file = "";
             var svgDocument = SvgDocument.Open(file);
             var bootstrapper = new DefaultBootstrapper();
-            var zplTransformer = bootstrapper.CreateZplTransformer();
-            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-            var zplRenderer = bootstrapper.CreateZplRenderer(zplTransformer,
-                                                             characterSet: CharacterSet.ZebraCodePage850);
-            var viewMatrix = bootstrapper.CreateViewMatrix(zplTransformer,
+            var fingerPrintTransformer = bootstrapper.CreateFingerPrintTransformer();
+            var fingerPrintRenderer = bootstrapper.CreateFingerPrintRenderer(fingerPrintTransformer);
+            var viewMatrix = bootstrapper.CreateViewMatrix(fingerPrintTransformer,
                                                            sourceDpi: 90f,
                                                            destinationDpi: 203f,
                                                            viewRotation: ViewRotation.Normal);
-            var zplContainer = zplRenderer.GetTranslation(svgDocument,
-                                                          viewMatrix);
+            var fingerPrintContainer = fingerPrintRenderer.GetTranslation(svgDocument,
+                                                                          viewMatrix);
 
-            var encoding = zplRenderer.GetEncoding();
-            var array = zplContainer.ToByteStream(encoding)
-                                    .ToArray();
+            var encoding = fingerPrintRenderer.GetEncoding();
+            var array = fingerPrintContainer.ToByteStream(encoding)
+                                            .ToArray();
 
-            var tttt =svgDocument.Draw();
-            svgDocument.Draw(tttt );
+            //var file = "C:\\Users\\Виктор\\OneDrive\\Рабочий стол\\Текстовый документ (2).SVG";
+            //var svgDocument = SvgDocument.Open(file);
+            //var bootstrapper = new DefaultBootstrapper();
+            //var zplTransformer = bootstrapper.CreateZplTransformer();
+            //Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            //var zplRenderer = bootstrapper.CreateZplRenderer(zplTransformer,
+            //                                                 characterSet: CharacterSet.ZebraCodePage850);
+            //var viewMatrix = bootstrapper.CreateViewMatrix(zplTransformer,
+            //                                               sourceDpi: 90f,
+            //                                               destinationDpi: 203f,
+            //                                               viewRotation: ViewRotation.Normal);
+            //var zplContainer = zplRenderer.GetTranslation(svgDocument,
+            //                                              viewMatrix);
+
+            //var encoding = zplRenderer.GetEncoding();
+            //var array = zplContainer.ToByteStream(encoding)
+            //                        .ToArray();
+            //string t = string.Empty;
 
 
+
+            //Console.WriteLine(array.ToString());
 
         }
     }
