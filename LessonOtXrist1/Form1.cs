29c9b385c0c5f270fda77ad5c770863014c747ab
Виктор Кизлик <kizlik1108@Gmail.com>
2022-09-19 15:55:31 +0300

    фывфыв

---
+++ LessonOtXrist1/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LessonOtXrist1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {            
+           DialogResult result =  MessageBox.Show( "Текст сообщения", "Заголовок",MessageBoxButtons.YesNo,MessageBoxIcon.Asterisk);
+
+            if (result== DialogResult.Yes)
+            {
+                MessageBox.Show("Нажали ДА");
+            }
+            if (result == DialogResult.No)
+            {
+                MessageBox.Show("Нажали НЕТ");
+            }
+
+        }
+    }
+}
